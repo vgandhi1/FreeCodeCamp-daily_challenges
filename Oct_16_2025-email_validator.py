eb83671ceb771d00898b167d5fb5717f19195805
vgandhi1 <vgandhi1@binghamton.edu>
2025-10-16 18:37:58 -0500

    Implement email validation function
    This script defines an email validation function that checks for specific constraints on email format, including the presence of one '@' symbol, valid characters in the local part, and proper structure in the domain part.

---
+++ Oct_16_2025-email_validator.py
@@ -0,0 +1,45 @@
+"""
+Email Validator
+Given a string, determine if it is a valid email address using the following constraints:
+
+It must contain exactly one @ symbol.
+The local part (before the @):
+Can only contain letters (a-z, A-Z), digits (0-9), dots (.), underscores (_), or hyphens (-).
+Cannot start or end with a dot.
+The domain part (after the @):
+Must contain at least one dot.
+Must end with a dot followed by at least two letters.
+Neither the local or domain part can have two dots in a row.
+"""
+
+def validate(email):
+    #email = "vinaygandhi@rivian.com"
+    exact_one_symbol = len(email.split("@"))==2
+    local_part = email.split("@")[0]
+    domain_part = email.split("@")[-1]
+    symbols = ['.','_','-']
+    #contains_symbol = any(symbol in local_part for symbol in symbols)
+    local_part_condition = all(char.isalnum() or char in symbols for char in local_part) and local_part[0] != "." and  local_part[-1] !="."
+    domain_part_condition = len(domain_part.split(".")) >= 2 and len(domain_part.split(".")[-1]) >= 2 and domain_part.split(".")[-1].isalpha()
+
+    double_dots = (not "" in local_part.split(".")) and (not "" in domain_part.split("."))
+
+    return exact_one_symbol and local_part_condition and domain_part_condition and double_dots
+
+#print(validate("freecodecamp.org"))
+
+#print( validate("a@b.cd"))
+#print(validate(".b@sh.rc"))
+
+"""
+Tests
+Waiting:1. validate("a@b.cd") should return True.
+Waiting:2. validate("hell.-w.rld@example.com") should return True.
+Waiting:3. validate(".b@sh.rc") should return False.
+Waiting:4. validate("example@test.c0") should return False.
+Waiting:5. validate("freecodecamp.org") should return False.
+Waiting:6. validate("develop.ment_user@c0D!NG.R.CKS") should return True.
+Waiting:7. validate("hello.@wo.rld") should return False.
+Waiting:8. validate("hello@world..com") should return False.
+Waiting:9. validate("git@commit@push.io") should return False.
+"""
