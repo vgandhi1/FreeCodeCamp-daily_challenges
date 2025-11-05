863ee1ac48cd4d827a449fc133845b7518099fb4
vgandhi1 <vgandhi1@binghamton.edu>
2025-11-04 20:57:35 -0600

    Create Nov_04_2025-image_search.py

---
+++ Nov_04_2025-image_search.py
@@ -0,0 +1,31 @@
+"""
+Image Search
+On November 4th, 2001, Google launched its image search, allowing people to find images using search terms. In this challenge, you will imitate the image search.
+
+Given an array of image names and a search term, return an array of image names containing the search term.
+
+Ignore the case when matching the search terms.
+Return the images in the same order they appear in the input array.
+"""
+
+def image_search(images, term):
+    split_images = [image.replace(".","").lower() for image in images]
+    lower_term = term.lower()
+    listed_images = []
+    for idx, image in enumerate(split_images):
+        if lower_term in image:
+            listed_images.append(images[idx])
+
+    return listed_images
+
+print(image_search(["Moon.png", "sun.jpeg", "stars.png"], "PNG"))
+print(image_search(["Sunset.jpg", "Beach.png", "sunflower.jpeg"], "sun"))
+
+
+"""
+Tests
+Passed:1. image_search(["dog.png", "cat.jpg", "parrot.jpeg"], "dog") should return ["dog.png"].
+Passed:2. image_search(["Sunset.jpg", "Beach.png", "sunflower.jpeg"], "sun") should return ["Sunset.jpg", "sunflower.jpeg"].
+Passed:3. image_search(["Moon.png", "sun.jpeg", "stars.png"], "PNG") should return ["Moon.png", "stars.png"].
+Passed:4. image_search(["cat.jpg", "dogToy.jpeg", "kitty-cat.png", "catNip.jpeg", "franken_cat.gif"], "Cat") should return ["cat.jpg", "kitty-cat.png", "catNip.jpeg", "franken_cat.gif"].
+"""
