a64d4f03e20ac3f4af06bfdf39daf5d5de0c11d3
vgandhi1 <vgandhi1@binghamton.edu>
2025-11-17 14:23:44 -0600

    Add fingerprint matching function and tests
    Implement fingerprint matching logic with test cases.

---
+++ Nov25/Nov_17_2025-fingerprint_test.py
@@ -0,0 +1,31 @@
+"""
+Fingerprint Test
+Given two strings representing fingerprints, determine if they are a match using the following rules:
+
+Each fingerprint will consist only of lowercase letters (a-z).
+Two fingerprints are considered a match if:
+They are the same length.
+The number of differing characters does not exceed 10% of the fingerprint length.
+"""
+def is_match(fingerprint_a, fingerprint_b):
+    a = len(fingerprint_a)
+    b = len(fingerprint_b)
+    diff = 0
+    if a == b:
+        for i, j in zip(fingerprint_a,fingerprint_b):
+            if i != j:
+               diff += 1
+        if diff <= 0.1*a:
+            return True
+
+    return False
+
+"""
+Tests
+Waiting:1. is_match("helloworld", "helloworld") should return True.
+Waiting:2. is_match("helloworld", "helloworlds") should return False.
+Waiting:3. is_match("helloworld", "jelloworld") should return True.
+Waiting:4. is_match("thequickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthelazydog") should return True.
+Waiting:5. is_match("theslickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthehazydog") should return True.
+Waiting:6. is_match("thequickbrownfoxjumpsoverthelazydog", "thequickbrownfoxjumpsoverthehazycat") should return False.
+"""
