87eb3135e3105583b2ed55ca46e2c58eac49fc5b
vgandhi1 <vgandhi1@binghamton.edu>
2025-11-23 10:43:24 -0600

    character counter

---
+++ Nov_23_2025-character_count.py
@@ -0,0 +1,40 @@
+"""
+Character Count
+Given a sentence string, return an array with a count of each character in alphabetical order.
+
+Treat upper and lowercase letters as the same letter when counting.
+Ignore numbers, spaces, punctuation, etc.
+Return the count and letter in the format "letter count". For instance, "a 3".
+All returned letters should be lowercase.
+Do not return a count of letters that are not in the given string.
+"""
+import re
+from collections import Counter
+def count_characters(sentence):
+    sentence = re.sub(r'[^\w\s]', '', sentence)
+    sentence = sentence.replace(" ","").lower()
+    
+    
+    sentence_list = []
+    freq = {}
+
+    freq_ = Counter(sentence)
+
+    for char in sentence:
+        freq[char] = freq.get(char, 0) + 1
+
+    freq_sorted = sorted(freq.items(), key=lambda item: item[0])
+
+    sentence_list = [f"{word} {count}" for word, count in freq_sorted]
+
+    return sentence_list
+print(count_characters("hello world"))
+#print(count_characters("// TODO: Complete this challenge ASAP!"))
+
+"""
+Tests
+
+Passed: 1. count_characters("hello world") should return ["d 1", "e 1", "h 1", "l 3", "o 2", "r 1", "w 1"].
+Passed: 2. count_characters("I love coding challenges!") should return ["a 1", "c 2", "d 1", "e 3", "g 2", "h 1", "i 2", "l 3", "n 2", "o 2", "s 1", "v 1"].
+Passed: 3. count_characters("// TODO: Complete this challenge ASAP!") should return ["a 3", "c 2", "d 1", "e 4", "g 1", "h 2", "i 1", "l 3", "m 1", "n 1", "o 3", "p 2", "s 2", "t 3"].
+"""
