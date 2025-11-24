d1236c00a79b67ed2dd0f4106a5e7f3dcecfc108
vgandhi1 <vgandhi1@binghamton.edu>
2025-11-24 06:42:14 -0600

    Add message validator function and tests
    Implement a message validator that checks if each word in a message starts with the corresponding letter in a validation string, considering case insensitivity.

---
+++ Nov25/Nov_24_2025-message_validator.py
@@ -0,0 +1,24 @@
+"""
+Message Validator
+Given a message string and a validation string, determine if the message is valid.
+
+A message is valid if each word in the message starts with the corresponding letter in the validation string, in order.
+Letters are case-insensitive.
+Words in the message are separated by single spaces.
+"""
+def is_valid_message(message, validation):
+    valid_message = "".join(m[0].lower() for m in message.split(" "))
+
+    return valid_message == validation.lower()
+
+print(is_valid_message("hello world", "hw"))
+print(is_valid_message("The quick brown fox jumps over the lazy dog.", "TQBFJOTLD"))
+
+"""
+Tests
+Passed:1. is_valid_message("hello world", "hw") should return True.
+Passed:2. is_valid_message("ALL CAPITAL LETTERS", "acl") should return True.
+Passed:3. is_valid_message("Coding challenge are boring.", "cca") should return False.
+Passed:4. is_valid_message("The quick brown fox jumps over the lazy dog.", "TQBFJOTLD") should return True.
+Passed:5. is_valid_message("The quick brown fox jumps over the lazy dog.", "TQBFJOTLDT") should return False.
+"""
