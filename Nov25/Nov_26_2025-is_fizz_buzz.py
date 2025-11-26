51b14ad56cf8becd7d1fa2519f21a60dfcaf5a79
vgandhi1 <vgandhi1@binghamton.edu>
2025-11-26 12:22:40 -0600

    improve the code for is_fuzz_bizz

--- Nov25/Nov_26_2025-is_fizz_buzz.py
+++ Nov25/Nov_26_2025-is_fizz_buzz.py
@@ -37,3 +37,65 @@ Passed: 5. is_fizz_buzz([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 
 Passed: 6. is_fizz_buzz([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, "Buzz"]) should return False.
 Passed: 7. is_fizz_buzz([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz", 37, 38, "Fizz", "Buzz", 41, "Fizz", 43, 44, "FizzBuzz", 46, 47, "Fizz", 49, "Buzz"]) should return True.
 """
+
+#alternative code
+def is_fizz_buzz(sequence):
+    """
+    Validates the sequence by checking each element against its expected value.
+    Returns False immediately if an error is found.
+    """
+    l = []
+    n = len(sequence)
+    
+
+    for i in range(1, n + 1):
+        expected = ""
+        if i%3 == 0:
+            expected += "Fizz"
+        if i%5 == 0:
+            expected += "Buzz"
+        
+        if not expected:
+            expected = i
+
+        l.append(expected)
+
+        if expected != sequence[i-1]:
+            return False
+
+    return True
+
+# Example Usage
+print(is_fizz_buzz([1, 2, "Fizz", 4]))
+
+#or better way
+
+def is_fizz_buzz(sequence):
+    """
+    Validates the sequence by checking each element against its expected value.
+    Returns False immediately if an error is found.
+    """
+    l = []
+    n = len(sequence)
+    
+
+    for i in range(1, n + 1):
+        expected = ""
+        if i%15 == 0:
+            expected = "FizzBuzz"
+        elif i%3 == 0:
+            expected = "Fizz"
+        elif i%5 == 0:
+            expected = "Buzz"
+        else:
+            expected = i
+
+        l.append(expected)
+
+        if expected != sequence[i-1]:
+            return False
+
+    return True
+
+# Example Usage
+print(is_fizz_buzz([1, 2, "Fizz", 4]))
