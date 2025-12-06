a7d0a417f810d28c4a58ae63e61d293bace7df7e
Vinay Gandhi <vgandhi1@binghamton.edu>
2025-12-05 23:26:49 -0600

    Add symmetric difference function and tests
    Implement symmetric difference function to return unique elements from two arrays.

---
+++ Dec25/Dec_05_2025-Symmetric_diff.py
@@ -0,0 +1,31 @@
+"""
+Symmetric Difference
+Given two arrays, return a new array containing the symmetric difference of them.
+
+The symmetric difference between two sets is the set of values that appear in either set, but not both.
+Return the values in the order they first appear in the input arrays.
+"""
+
+def difference(arr1, arr2):
+    diff = []
+    for i in arr1:
+        if i not in arr2:
+            diff.append(i)
+    for j in arr2:
+        if j not in arr1:
+            diff.append(j)
+
+
+    return diff
+
+print(difference([1, 2, 3], [3, 4, 5]))
+print(difference([1, 3, 5, 7, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+"""
+Tests
+Passed:1. difference([1, 2, 3], [3, 4, 5]) should return [1, 2, 4, 5].
+Passed:2. difference(["a", "b"], ["c", "b"]) should return ["a", "c"].
+Passed:3. difference([1, "a", 2], [2, "b", "a"]) should return [1, "b"].
+Passed:4. difference([1, 3, 5, 7, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]) should return [2, 4, 6, 8].
+
+"""
