f802213922dd595c0035a6f74917e3908679ef84
Vinay Gandhi <vgandhi1@binghamton.edu>
2025-12-14 08:09:32 -0600

    Add title_case function to capitalize words
    Implement a function to format strings in title case.

---
+++ Dec25/Dec_14_2025-capitalize_it.py
@@ -0,0 +1,26 @@
+"""
+Capitalize It
+Given a string title, return a new string formatted in title case using the following rules:
+
+Capitalize the first letter of each word.
+Make all other letters in each word lowercase.
+Words are always separated by a single space.
+"""
+
+def title_case(title):
+    title_list = title.lower().split(" ")
+    
+
+
+    return " ".join(word.capitalize() for word in title_list)
+
+print(title_case("hello world"))
+
+"""
+Tests
+
+Passed: 1. title_case("hello world") should return "Hello World".
+Passed: 2. title_case("the quick brown fox") should return "The Quick Brown Fox".
+Passed: 3. title_case("JAVASCRIPT AND PYTHON") should return "Javascript And Python".
+Passed: 4. title_case("AvOcAdO tOAst fOr brEAkfAst") should return "Avocado Toast For Breakfast".
+"""
