556296b3e5f0e0c9421423abc227b462b46fcf66
Vinay Gandhi <vgandhi1@binghamton.edu>
2025-12-28 01:12:58 -0600

    Refactor to_screaming_snake_case function
    Refactored to_screaming_snake_case function to use regex for better handling of camel case and hyphenated inputs.

--- Dec25/Dec_28_2025-SCREAMING_SNAKE_CASE.py
+++ Dec25/Dec_28_2025-SCREAMING_SNAKE_CASE.py
@@ -44,3 +44,27 @@ Passed:3. to_screaming_snake_case("user_id") should return "USER_ID".
 Passed:4. to_screaming_snake_case("user-address") should return "USER_ADDRESS".
 Passed:5. to_screaming_snake_case("username") should return "USERNAME".
 """
+
+def to_screaming_snake_case(variable_name):
+    variable_name = variable_name.replace("-", "_")
+
+    words = []
+    start = 0
+
+    for i in range(1, len(variable_name)):
+        if variable_name[i].isupper():
+            words.append(variable_name[start:i])
+            start = i
+
+    words.append(variable_name[start:])
+
+    
+
+    return "_".join(w.upper() for w in words if w)
+
+    import re
+
+def to_screaming_snake_case(name):
+    name = re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', name)
+    return name.replace("-", "_").upper()
+
